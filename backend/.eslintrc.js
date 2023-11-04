3f65ea562ba70d0c91a98c97f89b0b8ad2b9a81b
slimon <slimon0216@gmail.com>
2023-11-05 00:28:44 +0800

    Backend: Add linter and prettier

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,32 +1,38 @@
 module.exports = {
     env: {
-      es2021: true,
-      node: true,
+        es2021: true,
+        node: true,
     },
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-    overrides: [
-      {
-        env: {
-          node: true,
-        },
-        files: [".eslintrc.{js,cjs}"],
-        parserOptions: {
-          sourceType: "script",
-        },
-      },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
     ],
-    parser: "@typescript-eslint/parser",
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    plugins: ["@typescript-eslint"],
+    plugins: ['@typescript-eslint'],
     rules: {
-      "@typescript-eslint/consistent-type-imports": [
-        "warn",
-        { prefer: "type-imports", fixStyle: "separate-type-imports" },
-      ],
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+        '@typescript-eslint/consistent-type-imports': [
+            'warn',
+            { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+        ],
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_' },
+        ],
     },
-  };
-  
+};
--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,114 +1,120 @@
+import type {
+    CreateUserPayload,
+    CreateUserResponse,
+    GetUserResponse,
+    GetUsersResponse,
+    UpdateUserPayload,
+    deleteUserResponse,
+    updateUserResponse,
+} from '@lib/shared_types';
+import type { Request, Response } from 'express';
+
 import UserModel from '../models/user';
-import {genericErrorHandler, checkIdFormat} from '../utils/errors';
-
-import type {CreateUserPayload, CreateUserResponse, GetUserResponse, GetUsersResponse, UpdateUserPayload, updateUserResponse, deleteUserResponse} from '@lib/shared_types';
-
-import type {Request, Response} from 'express';
+import { checkIdFormat, genericErrorHandler } from '../utils/errors';
 
 export const getUsers = async (_: Request, res: Response<GetUsersResponse>) => {
-  try {
-    const dbUsers = await UserModel.find({});
+    try {
+        const dbUsers = await UserModel.find({});
 
-    return res.status(200).json(dbUsers);
-  } catch (err) {
-    genericErrorHandler(err, res);
-  }
+        return res.status(200).json(dbUsers);
+    } catch (err) {
+        genericErrorHandler(err, res);
+    }
 };
 
 export const getUser = async (
-    req: Request<{id: string}>,
-    res: Response<GetUserResponse|{error: string}>) => {
-  try {
-    const {id} = req.params;
-    checkIdFormat(id, res);
+    req: Request<{ id: string }>,
+    res: Response<GetUserResponse | { error: string }>,
+) => {
+    try {
+        const { id } = req.params;
+        checkIdFormat(id, res);
 
-    const dbUser = await UserModel.findById(id);
-    if (!dbUser) {
-      return res.status(404).json({error: 'User not found'});
+        const dbUser = await UserModel.findById(id);
+        if (!dbUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        return res.status(200).json(dbUser);
+    } catch (err) {
+        genericErrorHandler(err, res);
     }
-
-    return res.status(200).json(dbUser);
-  } catch (err) {
-    genericErrorHandler(err, res);
-  }
 };
 
 export const createUser = async (
     req: Request<never, never, CreateUserPayload>,
-    res: Response<CreateUserResponse|{error: string}>,
-    ) => {
-  try {
-    const {name, password, email, phone, role, birthday} = req.body;
+    res: Response<CreateUserResponse | { error: string }>,
+) => {
+    try {
+        const { name, password, email, phone, role, birthday } = req.body;
 
-    // check if the user name is already in the database
-    const userExists = await UserModel.exists({name});
-    if (userExists) {
-      return res.status(404).json({error: 'User already exists'});
+        // check if the user name is already in the database
+        const userExists = await UserModel.exists({ name });
+        if (userExists) {
+            return res.status(404).json({ error: 'User already exists' });
+        }
+
+        const user = new UserModel({
+            name,
+            password,
+            email,
+            phone,
+            role,
+            birthday,
+            verified: false,
+            created_at: new Date().toISOString(),
+            last_login: new Date().toISOString(),
+        });
+
+        await user.save();
+        return res.status(201).json({ id: user.id });
+    } catch (err) {
+        genericErrorHandler(err, res);
     }
-
-    const user = new UserModel({
-      name,
-      password,
-      email,
-      phone,
-      role,
-      birthday,
-      verified: false,
-      created_at: new Date().toISOString(),
-      last_login: new Date().toISOString(),
-    });
-
-
-    await user.save();
-    return res.status(201).json({id: user.id});
-  } catch (err) {
-    genericErrorHandler(err, res);
-  }
 };
 
 export const updateUser = async (
-  req: Request<{ id: string }, never, UpdateUserPayload>,
-  res: Response<updateUserResponse | {error: string}>
+    req: Request<{ id: string }, never, UpdateUserPayload>,
+    res: Response<updateUserResponse | { error: string }>,
 ) => {
-  try {
-    const {id} = req.params;
-    checkIdFormat(id, res);
+    try {
+        const { id } = req.params;
+        checkIdFormat(id, res);
 
+        const oldUser = await UserModel.findById(id);
 
-    const oldUser = await UserModel.findById(id);
+        if (!oldUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
 
-    if (!oldUser) {
-      return res.status(404).json({error: 'User not found'});
+        const payLoad = req.body;
+
+        await UserModel.findByIdAndUpdate(id, payLoad);
+
+        res.status(200).send('OK');
+    } catch (err) {
+        genericErrorHandler(err, res);
     }
-
-    const payLoad = req.body;
-    
-    await UserModel.findByIdAndUpdate(id, payLoad);
-
-    res.status(200).send("OK");
-  } catch (err) {
-    genericErrorHandler(err, res);
-  }
 };
 
 export const deleteUser = async (
-  req: Request<{ id: string }>,
-  res: Response<deleteUserResponse | {error: string}>
+    req: Request<{ id: string }>,
+    res: Response<deleteUserResponse | { error: string }>,
 ) => {
-  try {
-    const {id} = req.params;
-    checkIdFormat(id, res);
+    try {
+        const { id } = req.params;
+        checkIdFormat(id, res);
 
-    const oldUser = await UserModel.findById(id);
+        const oldUser = await UserModel.findById(id);
 
-    if (!oldUser) {
-      return res.status(404).json({error: 'User not found'});
+        if (!oldUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        await UserModel.findByIdAndDelete(id);
+
+        res.status(200).send('OK');
+    } catch (err) {
+        genericErrorHandler(err, res);
     }
-
-    await UserModel.findByIdAndDelete(id);
-
-    res.status(200).send("OK");
-  } catch (err) {
-    genericErrorHandler(err, res);
-  }
 };
