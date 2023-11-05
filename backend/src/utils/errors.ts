0b851bce1a2ef188fa34c160637919dd53789b22
slimon <slimon0216@gmail.com>
2023-11-05 15:56:22 +0800

    Backend: add UserRepository to meet 12-factor app
    Add Data Access Layer to make the database service loose-coupling

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -1,22 +1,12 @@
 import type { Response } from 'express';
-import { MongooseError } from 'mongoose';
 
 export const genericErrorHandler = (error: unknown, res: Response) => {
     console.error(error);
 
     // Check the type of error
-    if (error instanceof MongooseError) {
-        return res.status(500).json({ error: error.message });
-    } else if (error instanceof Error) {
+    if (error instanceof Error) {
         return res.status(500).json({ error: error.message });
     } else {
         return res.status(500).json(error);
-    }
-};
-
-// check the id format is correct for mongodb
-export const checkIdFormat = (id: string, res: Response) => {
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-        return res.status(404).json({ error: 'Invalid id format for mongodb' });
     }
 };
