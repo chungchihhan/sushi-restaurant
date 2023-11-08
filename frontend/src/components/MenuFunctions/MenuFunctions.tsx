f0290a0be1ab1dba279be7c067723fd4450d4692
chungchihhan <harryup2000@gmail.com>
2023-11-08 22:14:36 +0800

    menu buttons

--- frontend/src/components/MenuFunctions/MenuFunctions.tsx
+++ frontend/src/components/MenuFunctions/MenuFunctions.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './MenuFunctions.css';
-import StoresPage from './StoresPage';
+// import StoresPage from './StoresPage';
+import { Link } from 'react-router-dom';
 
 interface MenuProps {
     onClose: () => void;
@@ -14,13 +15,56 @@ const MenuFunctions: React.FC<MenuProps> = ({ onClose }) => {
     };
 
     return (
-        <div className="modal-overlay" onClick={handleOutsideClick}>
-            <div className="modal-content">
-                <StoresPage/>
-                <h2>Modal Window</h2>
-                <p>This is the content inside the modal window.</p>         
-                <button onClick={onClose}>Close</button>
-                
+        <div className="menu-button-overlay" onClick={handleOutsideClick}>
+            <div className="menu-button-content">
+                {/* <StoresPage/> */}
+                {/* <h2>Modal Window</h2> */}
+                <Link to="/" className='single-menu-button-link'>
+                    <div className='single-menu-button'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>  
+                        </svg>
+                        <span className='single-menu-button-font'>食客</span>
+                    </div>
+                </Link>
+                <Link to="/menu" className='single-menu-button-link'>
+                    <div className='single-menu-button'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="bi bi-passport" viewBox="0 0 16 16">
+                            <path d="M8 5a3 3 0 1 0 0 6 3 3 0 0 0 0-6ZM6 8a2 2 0 1 1 4 0 2 2 0 0 1-4 0Zm-.5 4a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5Z"/>
+                            <path d="M3.232 1.776A1.5 1.5 0 0 0 2 3.252v10.95c0 .445.191.838.49 1.11.367.422.908.688 1.51.688h8a2 2 0 0 0 2-2V4a2 2 0 0 0-1-1.732v-.47A1.5 1.5 0 0 0 11.232.321l-8 1.454ZM4 3h8a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Z"/>
+                        </svg>
+                        <span className='single-menu-button-font'>menu</span>
+                    </div>
+                </Link>
+                <Link to="/" className='single-menu-button-link'>
+                    <div className='single-menu-button'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                        </svg>
+                        <span className='single-menu-button-font'>logout</span>
+                    </div >
+                </Link>
+                <Link to="/" className='single-menu-button-link'>
+                    <div className='single-menu-button'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                        </svg>
+                        <span className='single-menu-button-font'>購物車</span>
+                    </div>
+                </Link>
+                <Link to="/" className='single-menu-button-link'>
+                    <div className='single-menu-button'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="bi bi-clipboard2-check" viewBox="0 0 16 16">
+                            <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5h3Z"/>
+                        <path d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-12Z"/>
+                        <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3Z"/>
+                        </svg>
+                        <span className='single-menu-button-font'>消費紀錄</span>
+                    </div>
+                </Link>
+                {/* <p>This is the content inside the modal window.</p>          */}
+                {/* <button onClick={onClose}>Close</button> */}
             </div>
         </div>
     );
