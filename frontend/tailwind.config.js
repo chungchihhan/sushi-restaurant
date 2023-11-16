d35262126f8d82dd3f7e2196451cd94019565d70
XiangYi <azxcv1705@gmail.com>
2023-11-16 12:58:45 +0800

    add new folders and tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#618bab",
+        menu: "#aacddd",
+        info: "#d5d1d1",
+        bar: "#f5f5f5"
+      },
+    },
+    fontFamily: {
+      sans: ["var(--noto-sans)", ...defaultTheme.fontFamily.sans],
+    },
+  },
+  plugins: [],
+}
