5e7b4f4c0e09d858046d117e650e27aa00498b1b
chillchillman <davie234593@gmail.com>
2023-11-22 21:29:34 +0800

    Tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,6 +16,7 @@ import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import SignInPage from "./pages/SignInPage/SignInPage";
 import UserPage from "./pages/UserPage/UserPage";
 import CartPage from "./pages/CartPage/CartPage";
+import CartPage from "./pages/CartPage/CartPage";
 
 const Website: React.FC = () => {
   return (
@@ -41,7 +42,6 @@ const Website: React.FC = () => {
           <Route path="/record" element={<RecordPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/cart" element={<CartPage />} />
-          <Route path="/signin" element={<SignInPage />} />
         </Routes>
         {/* other components that are not routing-related */}
       </Router>
