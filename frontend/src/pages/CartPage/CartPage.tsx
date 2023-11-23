9f97a8e5c51d1411525466eb72afb0040360b664
chillchillman <davie234593@gmail.com>
2023-11-23 11:08:06 +0800

    frontend:Cart

--- frontend/src/pages/CartPage/CartPage.tsx
+++ frontend/src/pages/CartPage/CartPage.tsx
@@ -7,13 +7,13 @@ const orders = [
     store: "藏壽司",
     meal: "炙燒壽司",
     cost: 100,
-    img: "/menufood_1_img.jpg",
+    img: "/menu_1_img.jpg",
   },
   {
     store: "游壽司",
     meal: "蛙式壽司",
     cost: 90,
-    img: "/menufood_1_img.jpg",
+    img: "/menu_2_img.jpg",
   },
   // {
   //   store: "壽司狼",
@@ -30,28 +30,28 @@ const CartPage = () => {
       
 
       <div className="order-record-overlay rounded-md p-8">
-        <div className="order-record-content  rounded-md grid gap-4 bg-white p-10">
+        <div className="order-record-content  rounded-md grid gap-4 bg-white p-20">
           {orders.map((order, index) => (
             <div
               className={`order ${
                 index % 2 === 0 ? "gray-background" : ""
-              } flex rounded-md h-80 pt-5 bg-info px-20 `}
+              } flex rounded-md h-80 pt-5 bg-info px-40 `}
               key={index}
             >
               <div className="relative rounded-md w-100">
                 
                 <div className="tags">
-                  <label>店名：</label>
-                  <div className="store-tag rounded-md p-2 left-30 bg-white">{order.store}</div>
+                  <label className="md:font-bold">店名：</label>
+                  <div className="store-tag relative rounded-md p-2 pr-20 bg-white">{order.store}</div>
                 </div>
 
-                <label className="">餐點：</label>
+                <label className="md:font-bold">餐點：</label>
                 <div className="meal-tag">
-                  <div className="meal relative top-0 left-30 rounded-md p-2 pr-20 bg-white">{order.meal}</div>
+                  <div className="meal relative rounded-md p-2 pr-20 bg-white">{order.meal}</div>
                 </div>
 
-                <label className="">數量：</label>
-                <div className="number relative left-30 rounded-md p-2  bg-white">
+                <label className="md:font-bold">數量：</label>
+                <div className="number relative rounded-md p-2 pr-20 bg-white">
                     <select className="number-dropdown w-20">
                       <option className="number-list" value="1"> 1 </option>
                       <option className="number-list" value="2"> 2 </option>
@@ -60,14 +60,14 @@ const CartPage = () => {
                     </select>
                 </div>
 
-                <label className="">金額：</label>
-                <div className="cost-tag rounded-md p-2 pr-20 bg-white">
+                <label className="md:font-bold">金額：</label>
+                <div className="cost-tag relative rounded-md p-2 pr-20 bg-white">
                   <div className="cost">${order.cost}</div>
                 </div>
 
                 </div>
               
-                <label className="relative left-20 ">備註：</label>
+                <label className="relative left-20 md:font-bold">備註：</label>
               <div className="relative left-20 top-10 ">
               <textarea
                     className="
@@ -91,14 +91,17 @@ const CartPage = () => {
 
                     placeholder="輸入備註"
                   ></textarea>
-
-                  <img src="{order.img}" alt="" />
+              </div>
+              
+              <div className=" bg-indigo-300 relative left-40 top-2 h-60 lg:w-60 rounded-md ">
+              <img src={order.img} alt="" className="objet-none rounded-md"/>
               </div>
 
+              <button className="del-button bg-slate-500 hover:bg-slate-400 text-white font-bold h-20 lg:w-24  relative top-28 left-48 rounded-md ">刪除</button>
             </div>
           ))}
-          <button className="shop-button ">繼續選購</button>
-          <button className="correct-button">確認下單</button>
+          <button className="shop-button bg-slate-300 hover:bg-blue-500 text-white font-bold lg:w-72 rounded-md ">繼續選購</button>
+          <button className="correct-button  bg-slate-300 hover:bg-blue-500 text-white font-bold lg:w-72 rounded-md ">確認下單</button>
           {/* <button onClick={onClose}>Close</button> */}
         </div>
       </div>
