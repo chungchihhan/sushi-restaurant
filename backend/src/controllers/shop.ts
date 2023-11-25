3ed7ceb20cbf4e755eb88318387a48ce53b6a61f
Ryo <allen125372@gmail.com>
2023-11-25 22:27:38 +0800

    run yarn format

--- backend/src/controllers/shop.ts
+++ backend/src/controllers/shop.ts
@@ -14,10 +14,9 @@ import type {
     UpdateShopPayload,
     UpdateShopResponse,
 } from '@lib/shared_types';
-
 import type { Request, Response } from 'express';
 
-import { OrderStatus,CategoryList } from '../../../lib/shared_types';
+import { CategoryList, OrderStatus } from '../../../lib/shared_types';
 import { genericErrorHandler } from '../utils/errors';
 import { MongoMealRepository } from './meal_repository';
 import { MongoOrderItemRepository } from './orderItem_repository';
@@ -64,13 +63,22 @@ export const getShopsCategory = async (
     try {
         const dbShops = await shopRepo.findAll();
 
-        const validCategories: CategoryList[] = Object.values(CategoryList).map(value => value as CategoryList);
-        const invalidShops = dbShops.filter(shop => !validCategories.includes(shop.category as CategoryList));
+        const validCategories: CategoryList[] = Object.values(CategoryList).map(
+            (value) => value as CategoryList,
+        );
+        const invalidShops = dbShops.filter(
+            (shop) => !validCategories.includes(shop.category as CategoryList),
+        );
 
-    if (invalidShops.length > 0) {
-      const invalidShopDetails = invalidShops.map(shop => `{shop_id: ${shop.id}, category: ${shop.category as CategoryList}}`);
-      throw new Error(`Invalid categories: ${invalidShopDetails}`);
-    }
+        if (invalidShops.length > 0) {
+            const invalidShopDetails = invalidShops.map(
+                (shop) =>
+                    `{shop_id: ${shop.id}, category: ${
+                        shop.category as CategoryList
+                    }}`,
+            );
+            throw new Error(`Invalid categories: ${invalidShopDetails}`);
+        }
 
         const categoryCounts = dbShops.reduce(
             (acc, shop) => {
