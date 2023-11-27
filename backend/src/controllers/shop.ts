104a625f911f2dd5a2ac61c48c5279eae145a946
鄭晴文 <vivian90218@gmail.com>
2023-11-27 21:30:58 +0800

    rename Imgur related config in .env

--- backend/src/controllers/shop.ts
+++ backend/src/controllers/shop.ts
@@ -413,9 +413,9 @@ export const uploadImage = async (
         }
 
         const client = new ImgurClient({
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN,
+            clientId: process.env.IMGUR_CLIENT_ID,
+            clientSecret: process.env.IMGUR_CLIENT_SECRET,
+            refreshToken: process.env.IMGUR_REFRESH_TOKEN,
         });
 
         const response = await client.upload({
