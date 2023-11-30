3010cdad6de911fe2e09a06dff422cd9f7ac3a89
chungchihhan <harryup2000@gmail.com>
2023-11-30 22:42:37 +0800

    user sign

---
+++ frontend/src/pages/MenuFoodPage/MenuFoodPage.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+import BlueSquareFix from "./_components/BlueSquareFix";
+
+export default function MenuFoodPage() {
+  return (
+    <>
+      <div>
+        <BlueSquareFix />
+      </div>
+    </>
+  );
+}
