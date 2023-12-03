3ab4cfad905f16a7164fbd2536933bdb6a600468
slimon <slimon0216@gmail.com>
2023-12-03 17:32:44 +0800

    yarn build

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ pushd .
 cd frontend
 pwd
 yarn install --network-timeout 1000000
+yarn global add serve
+yarn build
 popd
 
 echo "DEPLOYMENT_SOURCE: $DEPLOYMENT_SOURCE"
 echo "DEPLOYMENT_TARGET: $DEPLOYMENT_TARGET"
 
-rm -r $DEPLOYMENT_SOURCE/backend
-
-cp -r $DEPLOYMENT_SOURCE/* $DEPLOYMENT_TARGET/
+cp -r $DEPLOYMENT_SOURCE/frontend/build $DEPLOYMENT_TARGET/build
 
