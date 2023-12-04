f5fb9d163f2ccce07c75ac272cd5ba5180f3212e
slimon <slimon0216@gmail.com>
2023-12-05 02:19:53 +0800

    Add miss file

---
+++ backend/src/utils/redis.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import { Redis } from 'ioredis';
+
+dotenv.config();
+
+let redisClient: Redis | null = null;
+
+if (process.env.REDIS_URL) {
+    redisClient = new Redis(process.env.REDIS_URL);
+    console.log('Connected to Redis');
+}
+
+export default redisClient;
