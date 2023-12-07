b678c8d0fc37fd8dee7c8ca87aa3dc725d82f8d9
鄭晴文 <vivian90218@gmail.com>
2023-12-07 20:40:45 +0800

    fix status default in order model

--- backend/src/models/order.ts
+++ backend/src/models/order.ts
@@ -30,7 +30,7 @@ const OrderSchema = new mongoose.Schema<OrderDocument>(
             type: String,
             required: true,
             enum: Object.values(OrderStatus),
-            default: OrderStatus.CART,
+            default: OrderStatus.WAITING,
         },
         remark: { type: String, required: false },
     },
