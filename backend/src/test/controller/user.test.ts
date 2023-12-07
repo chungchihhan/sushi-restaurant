a34b14ebc354bcfb0b05cc4c35fd0886852d2e69
鄭晴文 <vivian90218@gmail.com>
2023-12-07 21:40:12 +0800

    fix

--- backend/src/test/controller/user.test.ts
+++ backend/src/test/controller/user.test.ts
@@ -14,31 +14,9 @@ describe('User Controller', () => {
             jsonSpy: sinon.SinonSpy,
             req: Request<never, never, CreateUserPayload>,
             res: Response<CreateUserResponse | { error: string }>,
-            userRepoStub: sinon.SinonStub;
+            userRepoFindByAccountStub: sinon.SinonStub;
 
         beforeEach(() => {
-            statusStub = sinon.stub();
-            jsonSpy = sinon.spy();
-            res = {
-                status: statusStub,
-                json: jsonSpy,
-            } as unknown as Response<CreateUserResponse | { error: string }>;
-            statusStub.returns(res);
-
-            userRepoStub = sinon.stub(
-                MongoUserRepository.prototype,
-                'findByAccount',
-            );
-            sinon.stub(UserModel.prototype, 'save').resolves({ id: 'some-id' });
-        });
-
-        afterEach(() => {
-            sinon.restore();
-        });
-
-        it('should create a new user if the account does not exist', async () => {
-            userRepoStub.resolves(null);
-
             req = {
                 body: {
                     account: 'testAccount',
@@ -51,26 +29,35 @@ describe('User Controller', () => {
                 },
             } as Request<never, never, CreateUserPayload>;
 
+            statusStub = sinon.stub();
+            jsonSpy = sinon.spy();
+            res = {
+                status: statusStub,
+                json: jsonSpy,
+            } as unknown as Response<CreateUserResponse | { error: string }>;
+            statusStub.returns(res);
+            userRepoFindByAccountStub = sinon.stub(
+                MongoUserRepository.prototype,
+                'findByAccount',
+            );
+            sinon.stub(UserModel.prototype, 'save').resolves({ id: 'some-id' });
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('should create a new user if the account does not exist', async () => {
+            userRepoFindByAccountStub.resolves(null);
+
             await createUser(req, res);
 
             expect(statusStub.calledWith(201)).to.be.true;
-            expect(jsonSpy.calledWith(sinon.match.has('id'))).to.be.true;
+            expect(jsonSpy.calledWith({ id: 'some-id' })).to.be.true;
         });
 
         it('should return an error if the user already exists', async () => {
-            userRepoStub.resolves(!null);
-
-            req = {
-                body: {
-                    account: 'existingAccount',
-                    username: 'existingUser',
-                    password: 'password',
-                    email: 'test@example.com',
-                    phone: '1234567890',
-                    role: 'user',
-                    birthday: '2000-01-01',
-                },
-            } as Request<never, never, CreateUserPayload>;
+            userRepoFindByAccountStub.resolves(!null);
 
             await createUser(req, res);
 
