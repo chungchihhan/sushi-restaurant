a9c057fb419ea604ee2d6f17c64c86aacbb2b417
chillchillman <davie234593@gmail.com>
2023-12-12 20:24:16 +0800

    add animate.css

--- frontend/src/pages/UserPage/UserPage.tsx
+++ frontend/src/pages/UserPage/UserPage.tsx
@@ -1,106 +1,151 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { useState, useEffect } from "react";
+import type { ChangeEvent } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
-import { getUser } from "../../utils/client";
-import { userLogin } from "../../utils/client";
-import "animate.css";
+import { getUser, editUser } from "../../utils/client";
 
-interface SignInFormData {
+interface UserFormData {
   account: string;
+  username: string;
+  email: string;
+  phone: string;
+  role: string;
+  birthday: string;
   password: string;
 }
 
-export default function SignInPage() {
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState<SignInFormData>({
+export default function UserPage() {
+  const [formData, setFormData] = useState<UserFormData>({
     account: "",
+    username: "",
+    email: "",
+    phone: "",
+    role: "",
+    birthday: "",
     password: "",
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem("userToken");
+        const userId = localStorage.getItem("userId");
+
+        if (token && userId) {
+          const config = {
+            headers: { Authorization: `Bearer ${token}` },
+          };
+          const res = await getUser(userId, config);
+          const transformedData: UserFormData = {
+            account: res.data.account,
+            username: res.data.username,
+            email: res.data.email,
+            phone: res.data.phone,
+            role: res.data.role,
+            birthday: res.data.birthday,
+            password: res.data.password,
+          };
+          setFormData(transformedData);
+        }
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("Error fetching user data", error.message);
+        }
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  const handleInputChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
     try {
-      const res = await userLogin(formData);
-      const token = res.data.token;
-      const userId = res.data.id;
-      const shopId = res.data.shop_id;
-      if (shopId && shopId !== "none") {
-        localStorage.setItem("shopId", shopId);
-        // 可以在這裡添加導航到店鋪相關頁面的邏輯
-      }
-
-      if (token) {
-        localStorage.setItem("userToken", token);
-        localStorage.setItem("userId", userId);
-        toast.success("User signed in successfully!");
-
-        // 調用 getUser 函數
-        const userResponse = await getUser(userId, {
+      const token = localStorage.getItem("userToken");
+      const userId = localStorage.getItem("userId");
+      if (token && userId) {
+        // 移除了 console.log
+        const config = {
           headers: { Authorization: `Bearer ${token}` },
-        });
-        if (userResponse && userResponse.data) {
-          localStorage.setItem("userRole", userResponse.data.role);
-          localStorage.setItem("username", userResponse.data.username);
-        }
-
-        navigate("/");
-        window.location.reload();
-      } else {
-        toast.error("No token received.");
+        };
+        await editUser(userId, formData, config);
+        toast.success("User updated successfully!");
       }
-      setFormData({ account: "", password: "" });
-    } catch (error) {
-      console.error(error);
-      toast.error("Error signing in.");
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error("Error updating user", error.message); // 使用 console.error
+      }
+      toast.error("Error updating user.");
     }
   };
+
+  const roles = ["店家", "食客"];
+
+  function getFormFieldValue(key: keyof UserFormData) {
+    return formData[key];
+  }
 
   return (
     <>
       <ToastContainer />
-      <div className="flex h-screen items-center justify-center">
-        <form
-          onSubmit={handleSubmit}
-          className="animate__animated animate__rubberBand mb-4 rounded-full bg-white px-8 pb-8 pt-6 shadow-md"
-          // style={{ maxWidth: '400px' }}
-        >
-          <p className="font-mono animate__animated animate__heartBeat mb-8 flex justify-center text-4xl font-bold">
-            Login
-          </p>
-          <div className="mb-4">
-            <input
-              type="text"
-              name="account"
-              value={formData.account}
-              onChange={handleChange}
-              placeholder="Name"
-              className="focus:shadow-outline animate__animated animate__bounce w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-            />
-          </div>
-          <div className="mb-6">
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="Password"
-              className="focus:shadow-outline animate__animated animate__shakeX mb-3 w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-            />
-          </div>
-          <div className="ml-20 flex justify-between">
-            <button
-              type="submit"
-              className=" animate__animated animate__tada rounded bg-blue-500 px-4 py-2 font-bold  text-white hover:bg-blue-700"
-            >
-              Sign In
-            </button>
-          </div>
+      <div className="mx-auto max-w-2xl rounded-lg bg-gray-300 p-8 shadow-lg">
+        <h1 className="mb-6 text-center text-2xl font-bold">Edit User</h1>
+
+        <form className="space-y-4">
+          {Object.keys(formData).map((key) => (
+            <div key={key}>
+              <label
+                className="block text-sm font-medium capitalize text-gray-700"
+                htmlFor={key}
+              >
+                {key}
+              </label>
+              {key === "role" ? (
+                <select
+                  id={key}
+                  name={key}
+                  value={formData.role}
+                  onChange={handleInputChange}
+                  className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                >
+                  {roles.map((role) => (
+                    <option key={role} value={role}>
+                      {role}
+                    </option>
+                  ))}
+                </select>
+              ) : key === "birthday" ? (
+                <input
+                  id={key}
+                  type="date"
+                  name={key}
+                  value={formData.birthday}
+                  onChange={handleInputChange}
+                  className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                />
+              ) : (
+                <input
+                  id={key}
+                  type="text"
+                  name={key}
+                  value={getFormFieldValue(key as keyof UserFormData)}
+                  onChange={handleInputChange}
+                  placeholder={key}
+                  className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                />
+              )}
+            </div>
+          ))}
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="w-full rounded-md bg-teal-700 px-4 py-2 font-semibold text-white shadow hover:bg-teal-800"
+          >
+            Save Changes
+          </button>
         </form>
       </div>
     </>
