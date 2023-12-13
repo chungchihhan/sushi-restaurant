1ca1993412eeae03cb45f1fc4d532e6be0714f1d
XiangYi <azxcv1705@gmail.com>
2023-12-13 11:57:17 +0800

    Frotend: Saler OrderPage RWD UI DONE

--- frontend/src/pages/OrderPage/SalerOrderPage/_components/SalerOrderItem.tsx
+++ frontend/src/pages/OrderPage/SalerOrderPage/_components/SalerOrderItem.tsx
@@ -10,102 +10,136 @@ type SalerOrderItemProps = {
 
 export default function SalerOrderItem({ order, shopId }: SalerOrderItemProps) {
   const [updatedStatus, setUpdatedStatus] = useState(order.status);
+  if (updatedStatus === "waiting") {
+    setUpdatedStatus("尚未確認");
+  } else if (updatedStatus === "inprogress") {
+    setUpdatedStatus("製作中");
+  } else if (updatedStatus === "finished") {
+    setUpdatedStatus("已完成");
+  } else if (updatedStatus === "cancelled") {
+    setUpdatedStatus("已取消");
+  }
+
   const [isStatusChangeButtonVisible, setIsStatusChangeButtonVisible] =
     useState(true);
   const [isCancelButtonVisible, setIsCancelButtonVisible] = useState(true);
 
   const handleStatusChange = () => {
     updateOrder(shopId, order.order_id, { status: "inprogress" });
-    setUpdatedStatus("inprogress");
+    setUpdatedStatus("製作中");
     setIsStatusChangeButtonVisible(false);
     setIsCancelButtonVisible(false);
   };
 
   const handleOrderCancelled = () => {
     updateOrder(shopId, order.order_id, { status: "cancelled" });
-    setUpdatedStatus("cancelled");
+    setUpdatedStatus("已取消");
     setIsStatusChangeButtonVisible(false);
     setIsCancelButtonVisible(false);
   };
 
   const handleStatusSelectChange = (
-    e: React.ChangeEvent<HTMLSelectElement>,
   ) => {
-    const selectedStatus = e.target.value;
-    updateOrder(shopId, order.order_id, { status: selectedStatus });
-    setUpdatedStatus(selectedStatus);
+    updateOrder(shopId, order.order_id, { status: "finished" });
+    setUpdatedStatus('已完成');
   };
 
+  function addHoursAndFormat(originalTime: string): string {
+    const originalDate = new Date(originalTime);
+    const newDate = new Date(originalDate.getTime());
+
+    const year = newDate.getFullYear();
+    const month = String(newDate.getMonth() + 1).padStart(2, '0');
+    const day = String(newDate.getDate()).padStart(2, '0');
+    const hours = String(newDate.getHours()).padStart(2, '0');
+    const minutes = String(newDate.getMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  } 
+
+
   let statusElement;
-  if (updatedStatus === "waiting") {
+  if (updatedStatus === "尚未確認") {
     statusElement = (
-      <div>
+      <div className="flex gap-4 items-center rounded-3xl justify-center">
         <button
-          className={`m-2 bg-green-500 px-4 py-2 text-white ${
+          className={`w-1/2 bg-blue-400 rounded-3xl p-2 text-white ${
             isStatusChangeButtonVisible ? "" : "hidden"
           }`}
           onClick={handleStatusChange}
         >
-          V
+          確認
         </button>
         <button
-          className={`m-2 bg-red-500 px-4 py-2 text-white ${
+          className={`w-1/2 bg-slate-400 rounded-3xl p-2 text-white ${
             isCancelButtonVisible ? "" : "hidden"
           }`}
           onClick={handleOrderCancelled}
         >
-          X
+          取消
         </button>
       </div>
     );
-  } else if (updatedStatus === "inprogress") {
+  } else if (updatedStatus === "製作中") {
     statusElement = (
-      <div>
-        <select value={updatedStatus} onChange={handleStatusSelectChange}>
-          <option value="inprogress">In Progress</option>
-          <option value="finished">Finished</option>
-        </select>
+      <div className="flex w-full items-center justify-center">
+        <button onClick={handleStatusSelectChange} className="w-1/2 rounded-3xl bg-blue-400 p-2">
+          完成
+        </button>
       </div>
     );
   }
 
   return (
     <div
-      className={`flex items-center bg-blue-300 ${
-        updatedStatus === "finished" || updatedStatus === "cancelled"
-          ? "grayscale"
-          : ""
+      className={`flex-col items-center rounded-2xl ${
+        updatedStatus === "已完成" || updatedStatus === "已取消"
+          ? "grayscale bg-slate-400"
+          : "bg-slate-900"
       }`}
     >
-      <div className="flex flex-col items-center rounded-md p-4">
-        <div className="tags flex gap-4 rounded-md">
-          <div className="date-tag bg-green-300 p-2 text-2xl font-bold">
-            {order.order_id}
+      <div className="flex w-full justify-between items-center p-4 gap-4 rounded-md">
+        <div className="w-2/5 rounded-lg text-center bg-slate-200 p-2 text-2xl font-bold">
+          單號: #{order.order_id}
+        </div>
+        <div className="w-1/5 rounded-lg text-center bg-slate-200 p-2 text-2xl font-bold">
+          {addHoursAndFormat(order.order_date)}
+        </div>
+        <div className="w-1/5 rounded-lg text-center bg-slate-200 p-2 text-2xl font-bold">
+          {updatedStatus}
+        </div>
+        <div className="w-1/5 rounded-lg text-center p-2 text-2xl font-bold">{statusElement}</div>
+      </div>
+      <div className="flex-col gap-4 rounded-md bg-info p-2 m-4">
+        <div className="flex gap-4 rounded-md bg-info py-2">
+          <div className="w-1/4 self-center rounded-lg bg-slate-400 p-2 text-center text-3xl font-bold">
+            品項
           </div>
-          <div className="status-tag bg-blue-400 p-2 text-2xl font-bold">
-            {updatedStatus}
+          <div className="w-1/4 self-center rounded-lg bg-slate-400 p-2 text-center text-3xl font-bold">
+            數量
           </div>
-          <div className="date-tag bg-green-300 p-2 text-2xl font-bold">
-            {order.order_date}
+          <div className="w-1/4 self-center rounded-lg bg-slate-400 p-2 text-center text-3xl font-bold">
+            金額
+          </div>
+          <div className="w-1/4 self-center rounded-lg bg-slate-400 p-2 text-center text-3xl font-bold">
+            備註
           </div>
         </div>
-        <div className="order-details m-2 text-2xl font-bold"></div>
-        <div className="order-details m-2 text-3xl font-bold"></div>
+        <div className="grid gap-4">
+          {order.order_items.map((item, index) => (
+            <div key={index} className="flex gap-4 rounded-md bg-info">
+              <div className="w-1/4 self-center rounded-lg bg-slate-200 p-2 text-center text-2xl font-bold">{item.meal_name}</div>
+              <div className="w-1/4 self-center rounded-lg bg-slate-200 p-2 text-center text-2xl font-bold">{item.quantity}</div>
+              <div className="w-1/4 self-center rounded-lg bg-slate-200 p-2 text-center text-2xl font-bold">{item.sum_price}</div>
+              <div className="w-1/4 self-center rounded-lg bg-slate-200 p-2 text-center text-2xl font-bold">{item.remark}</div>
+            </div>
+          ))}
+        </div>
+        <hr className="h-px my-2 border-0 dark:bg-slate-700"></hr>
+        <div className="flex justify-end rounded-lg p-4 text-3xl font-bold">
+          總金額:<span className="ml-4 underline">{order.total_price}</span>
+        </div>
       </div>
-      <div className="order-record-content grid gap-4">
-        {order.order_items.map((item, index) => (
-          <div key={index} className="order-details m-2 text-3xl font-bold">
-            <div className="amount">${item.meal_name}</div>
-            <div className="amount">${item.quantity}</div>
-            <div className="amount">${item.sum_price}</div>
-            <div className="amount">${item.remark}</div>
-          </div>
-        ))}
-      </div>
-      <div className="status-tag bg-blue-400 p-2 text-2xl font-bold">
-        {order.total_price}
-      </div>
-      {statusElement}
     </div>
   );
 }
