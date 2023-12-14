36dcf04fa6a189a65b9bf1c3304f553fcf1ff68b
chungchihhan <144231383+chungchihhan@users.noreply.github.com>
2023-12-14 22:49:30 +0800

    Frontend/userpage pw (#96)
    * usereditpage undone
    
    * UserPage done
    
    * lint and format

--- frontend/src/pages/UserPage/UserPage.tsx
+++ frontend/src/pages/UserPage/UserPage.tsx
@@ -100,63 +100,138 @@ export default function UserPage() {
     }
   };
 
-  const roles = ["店家", "食客"];
+  const [showPassword, setShowPassword] = useState<boolean>(false);
 
-  function getFormFieldValue(key: keyof UserFormData) {
-    return formData[key];
-  }
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <>
       <ToastContainer />
       <div className="mx-auto max-w-2xl rounded-lg bg-gray-300 p-8 shadow-lg">
         <h1 className="mb-6 text-center text-2xl font-bold">Edit User</h1>
-
-        <form className="space-y-4">
-          {Object.keys(formData).map((key) => (
-            <div key={key}>
-              <label
-                className="block text-sm font-medium capitalize text-gray-700"
-                htmlFor={key}
-              >
-                {key}
-              </label>
-              {key === "role" ? (
-                <select
-                  id={key}
-                  name={key}
-                  value={formData.role}
-                  onChange={handleInputChange}
-                  className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+        <div className="flex flex-col gap-10">
+          <div className="flex">
+            <form className="space-y-4">
+              {/* Role */}
+              <div className="flex items-center gap-2">
+                <label
+                  className="block w-20 text-sm font-medium text-gray-700"
+                  htmlFor="role"
                 >
-                  {roles.map((role) => (
-                    <option key={role} value={role}>
-                      {role}
-                    </option>
-                  ))}
-                </select>
-              ) : key === "birthday" ? (
+                  身分
+                </label>
+                <div>{formData.role}</div>
+              </div>
+              {/* Account */}
+              <div className="flex items-center gap-2">
+                <label
+                  className="block w-20 text-sm font-medium text-gray-700"
+                  htmlFor="account"
+                >
+                  Account
+                </label>
                 <input
-                  id={key}
+                  id="account"
+                  type="text"
+                  name="account"
+                  value={formData.account}
+                  onChange={handleInputChange}
+                  placeholder="account"
+                  className="rounded-lg p-2"
+                />
+              </div>
+
+              {/* Username */}
+              <div className="flex items-center gap-2">
+                <label
+                  className="block w-20 text-sm font-medium text-gray-700"
+                  htmlFor="username"
+                >
+                  Username
+                </label>
+                <input
+                  id="username"
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                  placeholder="username"
+                  className="rounded-lg p-2"
+                />
+              </div>
+
+              {/* Email */}
+              <div className="flex items-center gap-2">
+                <label
+                  className="block w-20 text-sm font-medium text-gray-700"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="text"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  placeholder="email"
+                  className="rounded-lg p-2"
+                />
+              </div>
+              {/* Password */}
+              <div className="flex items-center gap-2">
+                <label
+                  className="block w-20 text-sm font-medium text-gray-700"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <div className="flex items-center">
+                  <input
+                    id="password"
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    className="rounded-lg p-2"
+                  />
+                  <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="rounded-md px-3 py-1 text-sm text-gray-600 hover:bg-gray-300"
+                  >
+                    {showPassword ? "Hide" : "Show"}
+                  </button>
+                </div>
+              </div>
+
+              {/* Birthday */}
+              <div className="flex items-center gap-2">
+                <label
+                  className="block w-20 text-sm font-medium text-gray-700"
+                  htmlFor="birthday"
+                >
+                  Birthday
+                </label>
+                <input
+                  id="birthday"
                   type="date"
-                  name={key}
+                  name="birthday"
                   value={formData.birthday}
                   onChange={handleInputChange}
-                  className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                  className="w-52 rounded-lg p-2"
                 />
-              ) : (
-                <input
-                  id={key}
-                  type="text"
-                  name={key}
-                  value={getFormFieldValue(key as keyof UserFormData)}
-                  onChange={handleInputChange}
-                  placeholder={key}
-                  className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
-                />
-              )}
+              </div>
+            </form>
+
+            <div className="items-center">
+              <h2 className="text-lg font-semibold">Your Balance:</h2>
+              <p className="text-4xl">$ {balance.toFixed(1)}</p>
             </div>
-          ))}
+          </div>
+
           <button
             type="button"
             onClick={handleSubmit}
@@ -164,10 +239,6 @@ export default function UserPage() {
           >
             Save Changes
           </button>
-        </form>
-        <div className="mt-4">
-          <h2 className="text-lg font-semibold">Your Balance:</h2>
-          <p>$ {balance.toFixed(1)}</p>
         </div>
       </div>
     </>
