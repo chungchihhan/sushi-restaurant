67ebfb9d99204249080bbe52a7151fda9cb2d14d
chungchihhan <harryup2000@gmail.com>
2023-12-14 23:14:33 +0800

    filter active

--- frontend/src/pages/SellPage/ShopPage/ShopEditPage.tsx
+++ frontend/src/pages/SellPage/ShopPage/ShopEditPage.tsx
@@ -40,6 +40,7 @@ type MealFormData = {
   quantity: number;
   category: string;
   image: string;
+  active:boolean;
   id: string;
 };
 
@@ -129,15 +130,13 @@ export default function ShopEditPage() {
 
   const fetchMeals = async () => {
     try {
-      // localStorage.setItem("shopId", "657587d769bc273573a7c202");
       const shopId = localStorage.getItem("shopId");
       if (shopId) {
         const mealsData = await getMealsByShopId(shopId);
-        // console.log(123)
-        setMeals(mealsData.data as MealFormData[]);
+        const activeMeals = mealsData.data.filter(meal => meal.active); 
+        console.log(activeMeals)
+        setMeals(activeMeals as MealFormData[]);
       }
-      // const mealsData = await getMealsByShopId("656c173a7c2af88779d401da")
-      // setMeals(mealsData.data as MealFormData[]);
     } catch (error) {
       console.error("Error fetching meals", error);
     }
