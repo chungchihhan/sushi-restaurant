0857dd7d003ee78f1c11f7b011be6bcfca4366f2
chungchihhan <harryup2000@gmail.com>
2023-12-15 16:02:00 +0800

    change style of create meal

--- frontend/src/pages/SellPage/ShopPage/MealCreateModal.tsx
+++ frontend/src/pages/SellPage/ShopPage/MealCreateModal.tsx
@@ -45,7 +45,7 @@ export default function MealCreateModal({
   const [uploadedImageUrl, setUploadedImageUrl] = useState<string>("");
 
   const handleChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>,
   ) => {
     const { name, value } = event.target;
     if (name === "image") {
@@ -92,69 +92,95 @@ export default function MealCreateModal({
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      className="max-w-2xl mx-auto bg-white rounded-lg overflow-hidden mt-20"
+      overlayClassName="fixed inset-0 bg-gray-600 bg-opacity-50"
       contentLabel="Create Meal"
     >
-      <div className="bg-gray-300 p-8">
-        <h1 className="mb-4 text-2xl font-bold">Create Meal</h1>
-        <form className="grid gap-4" onSubmit={handleSubmit}>
-          <input
-            className="rounded border border-gray-300 p-2"
-            type="text"
-            name="name"
-            value={mealData.name}
-            onChange={handleChange}
-            placeholder="Meal Name"
-          />
-          <input
-            className="rounded border border-gray-300 p-2"
-            type="text"
-            name="description"
-            value={mealData.description}
-            onChange={handleChange}
-            placeholder="Description"
-          />
-          <select
-            className="rounded border border-gray-300 p-2"
-            name="category"
-            value={mealData.category}
-            onChange={handleChange}
-          >
-            <option value="">選擇類型</option>
-            <option value="人氣精選">人氣精選</option>
-            <option value="便宜划算">便宜划算</option>
-            <option value="健康養生">健康養生</option>
-          </select>
-          <input
-            className="rounded border border-gray-300 p-2"
-            type="number"
-            name="price"
-            value={mealData.price}
-            onChange={handleChange}
-            placeholder="Price"
-          />
-          <input
-            className="rounded border border-gray-300 p-2"
-            type="number"
-            name="quantity"
-            value={mealData.quantity}
-            onChange={handleChange}
-            placeholder="Quantity"
-          />
-          <input
-            className="rounded border border-gray-300 p-2"
-            type="file"
-            name="image"
-            onChange={handleChange}
-          />
-          {uploadedImageUrl && (
-            <img src={uploadedImageUrl} alt="Uploaded Meal" />
-          )}
-          <button
-            type="submit"
-            className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-          >
-            Create Meal
-          </button>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold text-gray-800 mb-6">新增餐點</h1>
+        <form className="grid grid-cols-2 gap-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-5">
+            <div className="flex items-center gap-5">
+              <div>餐點名稱</div>
+              <input
+                className="rounded border border-gray-300 p-2 flex-grow"
+                type="text"
+                name="name"
+                value={mealData.name}
+                onChange={handleChange}
+                placeholder="名稱"
+              />
+            </div>
+            <div className="flex items-center gap-5">
+              <div>餐點標籤</div>
+              <select
+                className="rounded border border-gray-300 p-2 flex-grow"
+                name="category"
+                value={mealData.category}
+                onChange={handleChange}
+              >
+                <option value="">選擇標籤</option>
+                <option value="人氣精選">人氣精選</option>
+                <option value="便宜划算">便宜划算</option>
+                <option value="健康養生">健康養生</option>
+              </select>
+            </div>
+            <div className="flex items-center gap-5">
+              <div>餐點價錢</div>
+              <input
+                className="rounded border border-gray-300 p-2 flex-grow"
+                type="number"
+                name="price"
+                value={mealData.price}
+                onChange={handleChange}
+                placeholder="價錢"
+              />
+            </div>
+            <div className="flex items-center gap-5">
+              <div>餐點庫存</div>
+              <input
+                className="rounded border border-gray-300 p-2 flex-grow"
+                type="number"
+                name="quantity"
+                value={mealData.quantity}
+                onChange={handleChange}
+                placeholder="庫存"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-5">
+            <div className="flex items-center gap-5">
+              <div>餐點描述</div>
+              <textarea
+                className="rounded border border-gray-300 p-2 flex-grow min-h-[220px]"
+                name="description"
+                value={mealData.description}
+                onChange={handleChange}
+                placeholder="描述"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="col-span-2" >
+              <input
+                className="rounded-md border border-gray-300 w-full p-1 file:rounded file:border-0 file:bg-gray-400 hover:bg-gray-100 file:text-white file:px-4 file:py-2 "
+                type="file"
+                name="image"
+                onChange={handleChange}
+              />
+              {uploadedImageUrl && (
+                <img className="mt-4 rounded-lg" src={uploadedImageUrl} alt="Uploaded Meal" />
+              )}
+            </div>
+              <button
+                type="submit"
+                className="mt-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+              >
+                儲存新增餐點
+              </button>
+          </div>  
         </form>
       </div>
     </Modal>
