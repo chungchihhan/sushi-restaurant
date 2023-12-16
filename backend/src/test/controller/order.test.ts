80f5a46d10ecec411caf5a878477fa44eaa6556b
鄭晴文 <vivian90218@gmail.com>
2023-12-16 16:43:29 +0800

    add unit test for order deleteOrder

--- backend/src/test/controller/order.test.ts
+++ backend/src/test/controller/order.test.ts
@@ -1,14 +1,25 @@
-import type { GetOrderResponse, GetOrdersResponse, CreateOrderPayload, CreateOrderResponse } from '@lib/shared_types';
+import type {
+    CreateOrderPayload,
+    CreateOrderResponse,
+    DeleteOrderResponse,
+    GetOrderResponse,
+    GetOrdersResponse,
+} from '@lib/shared_types';
 import { expect } from 'chai';
 import type { Request, Response } from 'express';
 import sinon from 'sinon';
 
-import { getOrder, getOrders, createOrder } from '../../controllers/order';
-import { MongoOrderRepository } from '../../controllers/order_repository';
-import { MongoUserRepository } from '../../controllers/user_repository';
-import { MongoShopRepository } from '../../controllers/shop_repository';
 import { MongoMealRepository } from '../../controllers/meal_repository';
+import {
+    createOrder,
+    deleteOrder,
+    getOrder,
+    getOrders,
+} from '../../controllers/order';
 import { MongoOrderItemRepository } from '../../controllers/orderItem_repository';
+import { MongoOrderRepository } from '../../controllers/order_repository';
+import { MongoShopRepository } from '../../controllers/shop_repository';
+import { MongoUserRepository } from '../../controllers/user_repository';
 
 describe('Order Controller', () => {
     describe('getOrders', () => {
@@ -158,14 +169,29 @@ describe('Order Controller', () => {
             res: Response<CreateOrderResponse | { error: string }>,
             statusStub: sinon.SinonStub,
             jsonSpy: sinon.SinonSpy;
-    
+
         beforeEach(() => {
-            userRepoFindByIdStub = sinon.stub(MongoUserRepository.prototype, 'findById');
-            shopRepoFindByIdStub = sinon.stub(MongoShopRepository.prototype, 'findById');
-            mealRepoFindByIdStub = sinon.stub(MongoMealRepository.prototype, 'findById');
-            orderRepoCreateStub = sinon.stub(MongoOrderRepository.prototype, 'create');
-            orderItemRepoCreateStub = sinon.stub(MongoOrderItemRepository.prototype, 'create');
-            
+            userRepoFindByIdStub = sinon.stub(
+                MongoUserRepository.prototype,
+                'findById',
+            );
+            shopRepoFindByIdStub = sinon.stub(
+                MongoShopRepository.prototype,
+                'findById',
+            );
+            mealRepoFindByIdStub = sinon.stub(
+                MongoMealRepository.prototype,
+                'findById',
+            );
+            orderRepoCreateStub = sinon.stub(
+                MongoOrderRepository.prototype,
+                'create',
+            );
+            orderItemRepoCreateStub = sinon.stub(
+                MongoOrderItemRepository.prototype,
+                'create',
+            );
+
             statusStub = sinon.stub();
             jsonSpy = sinon.spy();
             res = {
@@ -174,18 +200,20 @@ describe('Order Controller', () => {
             } as unknown as Response<CreateOrderResponse | { error: string }>;
             statusStub.returns(res);
         });
-    
+
         afterEach(() => {
             sinon.restore();
         });
-    
+
         it('should successfully create an order', async () => {
             userRepoFindByIdStub.withArgs('user1').resolves({ id: 'user1' });
             shopRepoFindByIdStub.withArgs('shop1').resolves({ id: 'shop1' });
-            mealRepoFindByIdStub.withArgs('meal1').resolves({ id: 'meal1', shop_id: 'shop1' });
+            mealRepoFindByIdStub
+                .withArgs('meal1')
+                .resolves({ id: 'meal1', shop_id: 'shop1' });
             orderRepoCreateStub.resolves({ id: 'order1' });
             orderItemRepoCreateStub.resolves({});
-        
+
             req = {
                 body: {
                     user_id: 'user1',
@@ -193,18 +221,18 @@ describe('Order Controller', () => {
                     order_items: [{ meal_id: 'meal1', quantity: 2 }],
                 },
             } as Request<never, never, CreateOrderPayload>;
-        
+
             await createOrder(req, res);
-        
+
             expect(orderRepoCreateStub.calledOnce).to.be.true;
             expect(orderItemRepoCreateStub.calledOnce).to.be.true;
             expect(statusStub.calledWith(201)).to.be.true;
             expect(jsonSpy.calledWith({ id: 'order1' })).to.be.true;
         });
-        
+
         it('should return 404 if the user is not found', async () => {
             userRepoFindByIdStub.withArgs('user1').resolves(null);
-        
+
             req = {
                 body: {
                     user_id: 'user1',
@@ -212,9 +240,9 @@ describe('Order Controller', () => {
                     order_items: [],
                 },
             } as unknown as Request<never, never, CreateOrderPayload>;
-        
+
             await createOrder(req, res);
-        
+
             expect(userRepoFindByIdStub.calledWith('user1')).to.be.true;
             expect(statusStub.calledWith(404)).to.be.true;
             expect(jsonSpy.calledWith({ error: 'User not found' })).to.be.true;
@@ -223,7 +251,7 @@ describe('Order Controller', () => {
         it('should return 404 if the shop is not found', async () => {
             userRepoFindByIdStub.withArgs('user1').resolves({ id: 'user1' });
             shopRepoFindByIdStub.withArgs('shop1').resolves(null);
-        
+
             req = {
                 body: {
                     user_id: 'user1',
@@ -231,20 +259,22 @@ describe('Order Controller', () => {
                     order_items: [],
                 },
             } as unknown as Request<never, never, CreateOrderPayload>;
-        
+
             await createOrder(req, res);
-        
+
             expect(shopRepoFindByIdStub.calledWith('shop1')).to.be.true;
             expect(statusStub.calledWith(404)).to.be.true;
             expect(jsonSpy.calledWith({ error: 'Shop not found' })).to.be.true;
         });
-        
+
         it('should return 404 if order creation fails', async () => {
             userRepoFindByIdStub.withArgs('user1').resolves({ id: 'user1' });
             shopRepoFindByIdStub.withArgs('shop1').resolves({ id: 'shop1' });
-            mealRepoFindByIdStub.withArgs('meal1').resolves({ id: 'meal1', shop_id: 'shop1' });
+            mealRepoFindByIdStub
+                .withArgs('meal1')
+                .resolves({ id: 'meal1', shop_id: 'shop1' });
             orderRepoCreateStub.resolves(null);
-    
+
             req = {
                 body: {
                     user_id: 'user1',
@@ -252,14 +282,15 @@ describe('Order Controller', () => {
                     order_items: [{ meal_id: 'meal1', quantity: 2 }],
                 },
             } as Request<never, never, CreateOrderPayload>;
-    
+
             await createOrder(req, res);
-    
+
             expect(userRepoFindByIdStub.calledWith('user1')).to.be.true;
             expect(shopRepoFindByIdStub.calledWith('shop1')).to.be.true;
             expect(orderRepoCreateStub.calledOnce).to.be.true;
             expect(statusStub.calledWith(404)).to.be.true;
-            expect(jsonSpy.calledWith({ error: 'Order creation failed' })).to.be.true;
+            expect(jsonSpy.calledWith({ error: 'Order creation failed' })).to.be
+                .true;
         });
 
         it('should return 404 if a meal in order items is not found', async () => {
@@ -267,7 +298,7 @@ describe('Order Controller', () => {
             shopRepoFindByIdStub.withArgs('shop1').resolves({ id: 'shop1' });
             orderRepoCreateStub.resolves(true);
             mealRepoFindByIdStub.withArgs('meal1').resolves(null);
-        
+
             req = {
                 body: {
                     user_id: 'user1',
@@ -275,22 +306,25 @@ describe('Order Controller', () => {
                     order_items: [{ meal_id: 'meal1', quantity: 2 }],
                 },
             } as Request<never, never, CreateOrderPayload>;
-        
+
             await createOrder(req, res);
-        
+
             expect(userRepoFindByIdStub.calledWith('user1')).to.be.true;
             expect(shopRepoFindByIdStub.calledWith('shop1')).to.be.true;
             expect(mealRepoFindByIdStub.calledWith('meal1')).to.be.true;
             expect(statusStub.calledWith(404)).to.be.true;
-            expect(jsonSpy.calledWith({ error: `Meal meal1 not found' ` })).to.be.true;
+            expect(jsonSpy.calledWith({ error: `Meal meal1 not found' ` })).to
+                .be.true;
         });
-        
+
         it('should return 404 if a meal in order items is not found in the specified shop', async () => {
             userRepoFindByIdStub.withArgs('user1').resolves({ id: 'user1' });
             shopRepoFindByIdStub.withArgs('shop1').resolves({ id: 'shop1' });
             orderRepoCreateStub.resolves(true);
-            mealRepoFindByIdStub.withArgs('meal1').resolves({ id: 'meal1', shop_id: 'shop2' }); // Meal belongs to a different shop
-        
+            mealRepoFindByIdStub
+                .withArgs('meal1')
+                .resolves({ id: 'meal1', shop_id: 'shop2' }); // Meal belongs to a different shop
+
             req = {
                 body: {
                     user_id: 'user1',
@@ -298,18 +332,22 @@ describe('Order Controller', () => {
                     order_items: [{ meal_id: 'meal1', quantity: 2 }],
                 },
             } as Request<never, never, CreateOrderPayload>;
-        
+
             await createOrder(req, res);
-        
+
             expect(mealRepoFindByIdStub.calledWith('meal1')).to.be.true;
             expect(statusStub.calledWith(404)).to.be.true;
-            expect(jsonSpy.calledWith({ error: `Meal meal1 not found in shop shop1` })).to.be.true;
+            expect(
+                jsonSpy.calledWith({
+                    error: `Meal meal1 not found in shop shop1`,
+                }),
+            ).to.be.true;
         });
-        
+
         it('should handle unexpected exceptions', async () => {
             const error = new Error('Unexpected error');
             userRepoFindByIdStub.withArgs('user1').throws(error); // Simulate an unexpected error
-        
+
             req = {
                 body: {
                     user_id: 'user1',
@@ -317,9 +355,95 @@ describe('Order Controller', () => {
                     order_items: [],
                 },
             } as unknown as Request<never, never, CreateOrderPayload>;
-        
+
             await createOrder(req, res);
         });
-        
+    });
+
+    describe('deleteOrder', () => {
+        let orderRepoFindByIdStub: sinon.SinonStub,
+            orderRepoDeleteByIdStub: sinon.SinonStub,
+            req: Request<{ id: string }>,
+            res: Response<DeleteOrderResponse | { error: string }>,
+            statusStub: sinon.SinonStub,
+            sendSpy: sinon.SinonSpy,
+            jsonSpy: sinon.SinonSpy;
+
+        beforeEach(() => {
+            statusStub = sinon.stub();
+            sendSpy = sinon.spy();
+            jsonSpy = sinon.spy();
+            res = {
+                status: statusStub,
+                send: sendSpy,
+                json: jsonSpy,
+            } as unknown as Response<DeleteOrderResponse | { error: string }>;
+            statusStub.returns(res);
+
+            orderRepoFindByIdStub = sinon.stub(
+                MongoOrderRepository.prototype,
+                'findById',
+            );
+            orderRepoDeleteByIdStub = sinon.stub(
+                MongoOrderRepository.prototype,
+                'deleteById',
+            );
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('should successfully delete an order', async () => {
+            const orderId = '1';
+            orderRepoFindByIdStub.withArgs(orderId).resolves({ id: orderId });
+            orderRepoDeleteByIdStub.withArgs(orderId).resolves(true);
+
+            req = { params: { id: orderId } } as Request<{ id: string }>;
+
+            await deleteOrder(req, res);
+
+            expect(orderRepoFindByIdStub.calledWith(orderId)).to.be.true;
+            expect(orderRepoDeleteByIdStub.calledWith(orderId)).to.be.true;
+            expect(statusStub.calledWith(200)).to.be.true;
+            expect(sendSpy.calledWith('OK')).to.be.true;
+        });
+
+        it('should return 404 if the order is not found', async () => {
+            const orderId = '1';
+            orderRepoFindByIdStub.withArgs(orderId).resolves(null);
+
+            req = { params: { id: orderId } } as Request<{ id: string }>;
+
+            await deleteOrder(req, res);
+
+            expect(orderRepoFindByIdStub.calledWith(orderId)).to.be.true;
+            expect(statusStub.calledWith(404)).to.be.true;
+            expect(jsonSpy.calledWith({ error: 'Order not found' })).to.be.true;
+        });
+
+        it('should return 404 if deletion fails', async () => {
+            const orderId = '1';
+            orderRepoFindByIdStub.withArgs(orderId).resolves({ id: orderId });
+            orderRepoDeleteByIdStub.withArgs(orderId).resolves(false);
+
+            req = { params: { id: orderId } } as Request<{ id: string }>;
+
+            await deleteOrder(req, res);
+
+            expect(orderRepoDeleteByIdStub.calledWith(orderId)).to.be.true;
+            expect(statusStub.calledWith(404)).to.be.true;
+            expect(jsonSpy.calledWith({ error: 'Delete fails' })).to.be.true;
+        });
+
+        it('should handle unexpected exceptions', async () => {
+            const error = new Error('Unexpected error');
+            const orderId = '1';
+            orderRepoFindByIdStub.withArgs(orderId).throws(error);
+
+            req = { params: { id: orderId } } as Request<{ id: string }>;
+
+            await deleteOrder(req, res);
+        });
     });
 });
