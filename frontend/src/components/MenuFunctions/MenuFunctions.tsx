69d2c54e8f9d63aee73263df0dc3d59ce10fe1e8
XiangYi <azxcv1705@gmail.com>
2023-12-17 12:39:35 +0800

    linter & prettier check

--- frontend/src/components/MenuFunctions/MenuFunctions.tsx
+++ frontend/src/components/MenuFunctions/MenuFunctions.tsx
@@ -90,7 +90,9 @@ const MenuFunctions: React.FC<MenuProps> = ({ onClose }) => {
                 <path d="M8 5a3 3 0 1 0 0 6 3 3 0 0 0 0-6ZM6 8a2 2 0 1 1 4 0 2 2 0 0 1-4 0Zm-.5 4a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5Z" />
                 <path d="M3.232 1.776A1.5 1.5 0 0 0 2 3.252v10.95c0 .445.191.838.49 1.11.367.422.908.688 1.51.688h8a2 2 0 0 0 2-2V4a2 2 0 0 0-1-1.732v-.47A1.5 1.5 0 0 0 11.232.321l-8 1.454ZM4 3h8a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Z" />
               </svg>
-              <span className="single-menu-button-font font-bold">我的商店</span>
+              <span className="single-menu-button-font font-bold">
+                我的商店
+              </span>
             </div>
           </Link>
         ) : (
@@ -213,7 +215,9 @@ const MenuFunctions: React.FC<MenuProps> = ({ onClose }) => {
                 <path d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-12Z" />
                 <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3Z" />
               </svg>
-              <span className="single-menu-button-font font-bold">消費紀錄</span>
+              <span className="single-menu-button-font font-bold">
+                消費紀錄
+              </span>
             </div>
           </Link>
         )}
