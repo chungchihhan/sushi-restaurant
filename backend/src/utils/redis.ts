c55bc9608b6e4c62a3900413bca9369f162f6288
slimon <45115446+slimon0216@users.noreply.github.com>
2023-12-18 19:52:53 +0800

    Mock redis (#136)

--- backend/src/utils/redis.ts
+++ backend/src/utils/redis.ts
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv';
 import Redis from 'ioredis';
+import redisMock from 'ioredis-mock';
 
 dotenv.config();
 
 let redisClient: Redis | null = null;
 
-if (process.env.NODE_ENV !== 'test' && process.env.REDIS_URL) {
+if (process.env.NODE_ENV === 'test') {
+    redisClient = new redisMock();
+    console.log('Connected to Redis Mock');
+} else if (process.env.NODE_ENV !== 'test' && process.env.REDIS_URL) {
     redisClient = new Redis(process.env.REDIS_URL);
     console.log('Connected to Redis');
 }
