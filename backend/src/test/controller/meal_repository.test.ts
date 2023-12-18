a2d27616d29827d647b4d1c9b68fc90c28586319
slimon <slimon0216@gmail.com>
2023-12-18 19:32:33 +0800

    Mock redis

--- backend/src/test/controller/meal_repository.test.ts
+++ backend/src/test/controller/meal_repository.test.ts
@@ -3,6 +3,7 @@ import sinon from 'sinon';
 
 import { MongoMealRepository } from '../../controllers/meal_repository';
 import MealModel from '../../models/meal';
+import redis from '../../utils/redis';
 
 describe('MongoMealRepository', () => {
     let mealRepository: MongoMealRepository,
@@ -10,7 +11,9 @@ describe('MongoMealRepository', () => {
         saveStub: sinon.SinonStub,
         findByIdStub: sinon.SinonStub,
         existsStub: sinon.SinonStub,
-        findByIdAndUpdateStub: sinon.SinonStub;
+        findByIdAndUpdateStub: sinon.SinonStub,
+        redisGetStub: sinon.SinonStub,
+        redisSetStub: sinon.SinonStub;
 
     beforeEach(() => {
         mealRepository = new MongoMealRepository();
@@ -19,6 +22,8 @@ describe('MongoMealRepository', () => {
         findByIdStub = sinon.stub(MealModel, 'findById');
         existsStub = sinon.stub(MealModel, 'exists');
         findByIdAndUpdateStub = sinon.stub(MealModel, 'findByIdAndUpdate');
+        redisGetStub = sinon.stub(redis as any, 'get');
+        redisSetStub = sinon.stub(redis as any, 'set');
     });
 
     afterEach(() => {
@@ -129,6 +134,63 @@ describe('MongoMealRepository', () => {
 
         expect(findByIdStub.calledWith('1')).to.be.true;
         expect(mealResponse).to.deep.equal(null);
+    });
+
+    it('findById should return cached meal from Redis', async () => {
+        const mockMeal = {
+            _id: '1',
+            shop_id: 'S1',
+            name: 'N1',
+            description: 'none',
+            price: 100,
+            quantity: 50,
+            category: '中式',
+            image: 'none',
+            active: true,
+        };
+        redisGetStub.resolves(JSON.stringify(mockMeal));
+
+        const result = await mealRepository.findById('1');
+        console.log(result);
+
+        expect(result).to.eql(mockMeal);
+        sinon.assert.calledOnce(redisGetStub);
+        sinon.assert.notCalled(findByIdStub);
+    });
+
+    it('should fetch meal from DB and cache it if not in Redis', async () => {
+        redisGetStub.resolves(null);
+        const dbMeal = {
+            _id: '1',
+            shop_id: 'S1',
+            name: 'N1',
+            description: 'none',
+            price: 100,
+            quantity: 50,
+            category: '中式',
+            image: 'none',
+            active: true,
+        };
+        findByIdStub.resolves(dbMeal);
+
+        const result = await mealRepository.findById('1');
+
+        // `_id` would become `id`
+        expect(result).to.include({ id: '1', name: 'N1' });
+        sinon.assert.calledOnce(redisGetStub);
+        sinon.assert.calledOnce(findByIdStub);
+        sinon.assert.calledOnce(redisSetStub);
+    });
+
+    it('should return null if meal not found in Redis or DB', async () => {
+        redisGetStub.resolves(null);
+        findByIdStub.resolves(null);
+
+        const result = await mealRepository.findById('1');
+
+        expect(result).to.be.null;
+        sinon.assert.calledOnce(redisGetStub);
+        sinon.assert.calledOnce(findByIdStub);
     });
 
     it('findByName should retrieve meals by name', async () => {
